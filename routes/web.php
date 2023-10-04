3032e0dcd021077e23b609d33cf2c0518885b4ff
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-10-04 10:08:39 +0700

    adjusment login register + dasboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/login', function () {
-    return view('login');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
 });
 
-
-Route::get('/register', function () {
-    return view('register');
-});
+require __DIR__.'/auth.php';
