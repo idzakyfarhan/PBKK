d988d40d879f9537e85a349f5c5c85975b0f6a57
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-12-09 03:28:35 +0700

    clean comment

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -9,25 +9,16 @@ use App\Models\Likes;
 
 class LikesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store($id)
     {
         $userId = auth()->id();
@@ -43,33 +34,21 @@ class LikesController extends Controller
         return back();
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Likes $likes)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Likes $likes)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Likes $likes)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Likes $likes)
     {
         //
