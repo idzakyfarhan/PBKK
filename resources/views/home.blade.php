25061728bbbee2d4cc7ff967c4cb4879fd27a1b1
NaufalIhza17 <nawfal17syahzada@gmail.com>
2023-12-13 23:16:28 +0700

    finishing #1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
-            <input type="text" name="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#2AA9E0] focus:border-[#2AA9E0]" placeholder="Search tweet..." required>
+            <input type="text" name="search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#2AA9E0] focus:border-[#2AA9E0]" placeholder="Search tweet or user..." required>
             <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-[#2AA9E0] hover:bg-[#317c9c] focus:ring-4 focus:outline-none focus:ring-[#2AA9E0] font-medium rounded-lg text-sm px-4 py-2">Search</button>
         </div>
     </form>
